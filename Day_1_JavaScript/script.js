ee0fd894247bf0cadeac31761d55a04ca28a28a6
raunak2904 <raunak2904@gmail.com>
2026-09-17 22:35:41 +0530

    day1 of javascript (variables)

--- Day_1_JavaScript/script.js
+++ Day_1_JavaScript/script.js
@@ -1,3 +1,9 @@
-console.log("Hello, World!");
-console.warn("this is warning");
-console.error("this is error");
+var a = 10
+
+var b = 20
+
+console.log('value of a is',a, 'value of b is' ,b);
+console.log('sum of a and b is',a+b);
+console.log('subtraction of a and b is',a-b);
+console.log('multiplication of a and b is',a*b);
+console.log('division of a and b is',a/b);
