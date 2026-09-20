b229efd0cd8f4cbe224eca93a8e602d1a6390065
raunak2904 <raunak2904@gmail.com>
2026-09-20 21:02:48 +0530

    day1 of javascript complete

--- Day_1_JavaScript/script.js
+++ Day_1_JavaScript/script.js
@@ -1,2 +1,16 @@
-var ans = confirm('are you a man??')
-console.log(ans);
+var user = prompt('Enter your name...')
+
+console.log(user);
+// prompt ke through kuchh puchha jaye to use javascript string ke jaise treat karta hai.🤗
+
+
+var a = "Raj";
+var b = 10;
+
+console.log(a+b);
+
+
+var a = "Raushan";
+var b = 578;
+
+console.log(a*b);
