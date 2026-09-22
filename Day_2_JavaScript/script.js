15d528880454118f8b2de905ce8a9d702d369e9a
raunak2904 <raunak2904@gmail.com>
2026-09-22 20:17:44 +0530

    day2 of javascript

---
+++ Day_2_JavaScript/script.js
@@ -0,0 +1,26 @@
+// var a ='10';
+// var b = 20;
+
+// console.log(a*b);
+
+
+
+// var a = Number(prompt('Number 1'))
+// var b = Number(prompt('Number 2'))
+
+// console.log(a+b);
+
+
+
+// var a = 10;
+// a += 5
+// console.log(a);
+
+var a = 10;
+var b = 20;
+
+// console.log(a==b);
+
+// console.log(a!=b);
+
+console.log(a >= b);
